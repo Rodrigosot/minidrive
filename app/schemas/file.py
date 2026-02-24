549eb48bd49ae06bdd915148ff21e97ada1f083d
RodrigoSotelo <roddriv430@gmail.com>
2026-02-23 22:17:25 -0800

    feat(files): add ShareFileSchema

--- app/schemas/file.py
+++ app/schemas/file.py
@@ -1,7 +1,16 @@
+from typing import Optional
 import uuid
 
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 class FileRename(BaseModel):
     id: uuid.UUID
     name: str
+
+class ShareFileSchema(BaseModel):
+    email: EmailStr  
+    expires_in_days: Optional[int] = 0
+    expires_in_hours: Optional[int] = 0
+    expires_in_minutes: Optional[int] = 0
+    forever: Optional[bool] = False
+
