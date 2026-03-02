6e7efa6936a461144f45cd120e79dad3b194690c
RodrigoSotelo <roddriv430@gmail.com>
2026-03-01 19:38:02 -0800

    feat: add logs for folder endpoints

--- app/services/folder_service.py
+++ app/services/folder_service.py
@@ -1,9 +1,12 @@
 from datetime import datetime, timezone
 
+from fastapi import Request
 from sqlalchemy.orm import Session
 from app.models.fileshare import FileShare
 from app.models.folder import Folder
 from app.models.file import File
+from app.models.user import User
+from app.services.activitylog_service import ActivityLogService, ActivityAction, TargetType
 from app.services.file_service import delete_file_from_b2
     
 def delete_folder_recursive(folder: Folder, db: Session):
@@ -21,18 +24,35 @@ def delete_folder_recursive(folder: Folder, db: Session):
     
     db.delete(folder)
 
-def soft_delete_folder_recursive(folder: Folder, db: Session):
+def soft_delete_folder_recursive(folder: Folder, db: Session, current_user: User,request: Request):
     files = db.query(File).filter(File.folder_id == folder.id).all()
 
     for file in files:
         file.deleted_at = datetime.now(timezone.utc)
+        ActivityLogService.log(db=db, user_id=current_user.id, action=ActivityAction.SOFT_DELETE_FILE, target_type=TargetType.FILE, target_id=file.id, details=
+        f"Soft delete file due to recursive folder deletion | "
+        f"file='{file.name}' ({file.id}) | "
+        f"parent_folder='{folder.name}' ({folder.id})", ip_address=request.client.host)
 
     subfolders = db.query(Folder).filter(Folder.parent_id == folder.id).all()
 
     for sub in subfolders:
-        soft_delete_folder_recursive(sub, db)
+        soft_delete_folder_recursive(sub, db, current_user, request)
 
     folder.deleted_at = datetime.now(timezone.utc)
+
+    ActivityLogService.log(
+        db=db,
+        user_id=current_user.id,
+        action=ActivityAction.SOFT_DELETE_FOLDER,
+        target_type=TargetType.FOLDER,
+        target_id=folder.id, 
+        details=(
+            f"Soft delete folder recursively | "
+            f"folder='{folder.name}' ({folder.id})"
+        ),
+        ip_address=request.client.host
+    )
 
 def recovery_folder(folder: Folder, db: Session):
     files = db.query(File).filter(File.folder_id == folder.id).all()
